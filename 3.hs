11acda4902ea134be0c135914f40d14208f7fa89
amkkun <amkkun@gmail.com>
2012-10-11 08:55:47 +0900

    333

--- 3.hs
+++ 3.hs
@@ -1,5 +1,21 @@
-primes :: [Int]
-primes = filter isPrime [1..]
-
 isPrime :: Int -> Bool
-isPrime 
+isPrime n = all (\m -> n `mod` m /= 0) [2..(n - 1)]
+
+solve :: Int
+solve = maximum $ primeFactors 600851475143
+
+primeFactors :: Int -> [Int]
+primeFactors n = primeFactors' n 2
+  where
+    primeFactors' num prime
+      | num < prime = []
+      | isPrime num = [num]
+      | otherwise = if num `mod` prime == 0
+                    then prime : primeFactors' (num `div` prime) prime
+                    else primeFactors' num (nextPrime prime)
+                       
+nextPrime :: Int -> Int
+nextPrime n
+  | isPrime $ succ n = succ n
+  | otherwise = nextPrime $ succ n
+ 
